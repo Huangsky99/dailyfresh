ddb6cd4819f1b843c4d46faa53b0d6ce6cda6269
huangsky <huangsky_99@163.com>
2018-07-12 22:48:42 +0800

    register function phase 1

--- df_user/views.py
+++ df_user/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from models import UserInfo, AddressInfo
 
 
 # Create your views here.
 def register(request):
-    return render(request, 'df_user/register.html')
+    if request.method == 'GET':
+        return render(request, 'df_user/register.html')
+    else:
+        body = request.body
+        uname = body.get('user_name')
+        upwd = body.get('pwd')
+        upwd2 = body.get('cpwd')
+        uemail = body.get('email')
