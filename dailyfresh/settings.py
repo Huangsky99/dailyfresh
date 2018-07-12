0e454ebc95b50224c5ad1516849f8064e4c1c10c
huangsky <huangsky_99@163.com>
2018-07-12 21:04:36 +0800

    templates init

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'dailyfresh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'root1234',
+        'NAME': 'dailyfresh',
     }
 }
 
@@ -101,3 +105,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
