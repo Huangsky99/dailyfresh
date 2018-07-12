c00dbe1b419507dc3047a994b4f9afd4f3000cdc
huangsky <huangsky_99@163.com>
2018-07-12 21:36:08 +0800

    define the userInfo and addressInfo

--- df_user/models.py
+++ df_user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class UserInfo(models.Model):
+    uname = models.CharField(max_length=20)
+    upwd = models.CharField(max_length=32)
+    uemail = models.CharField(max_length=20)
+
+
+class AddressInfo(models.Model):
+    auser = models.ForeignKey(UserInfo)
+    ashou = models.CharField(max_length=50)
+    aadd = models.CharField(max_length=100)
+    aphone = models.CharField(max_length=11)
+    ayoubian = models.CharField(max_length=6)
