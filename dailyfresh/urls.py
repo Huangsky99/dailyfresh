36d5a44e9dc69b537de65293f7347aa2a58fefaf
huangsky <huangsky_99@163.com>
2018-07-14 17:59:00 +0800

    add the url of df_street module

--- dailyfresh/urls.py
+++ dailyfresh/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from df_street import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user/', include('df_user.urls')),
+    url(r'^$', views.index),
+    url(r'^street/', include('df_street.urls')),
 ]
